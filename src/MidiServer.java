1bf7a9d05d8601625c8cca46cd8f3ef5d247f6ea
Mark Drobnak <mtd8050@rit.edu>
2018-04-25 14:58:32 -0400

    Add documentation and remove the handshake
    The server already does a TCP handshake with the client
    
    Signed-off-by: Mark Drobnak <mtd8050@rit.edu>

--- src/MidiServer.java
+++ src/MidiServer.java
@@ -7,8 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MidiServer {
-    static int HANDSHAKE = 50; //this is a placeholder
-    static int EOF = 0xFF; //more placeholders
+    private static int EOF = 0xFF; // placeholder
 
     private Sequencer sequencer;
     private BufferedReader reader;
@@ -16,20 +15,43 @@ public class MidiServer {
     private Track track;
     private File file;
 
+    public static void main(String[] args) {
+        new MidiServer("test.mid").run();
+    }
+
+    /**
+     * Create a new MidiServer
+     *
+     * @param filename the filename to write the new midi data to
+     */
     private MidiServer(String filename) {
        file = new File(filename);
     }
 
+    /**
+     * Start the server to listen for data and play it back
+     */
     private void run() {
         try {
+            // Setup the MIDI system
             sequencer = MidiSystem.getSequencer();
             sequence = new Sequence(Sequence.PPQ, 4);
+
+            // Start the TCP server
             ServerSocket server = new ServerSocket(1337);
+
+            // Wait for a client
             Socket socket = server.accept();
+
+            // Setup the reader and track for data transfer
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             track = sequence.createTrack();
+
+            // Transfer data
             readIn();
             writeToFile();
+
+            // Play the MIDI
             play();
         }
         catch(IOException | MidiUnavailableException | InvalidMidiDataException e) {
@@ -37,47 +59,60 @@ public class MidiServer {
         }
     }
 
+    /**
+     * Read in the MIDI data from the client
+     *
+     * @throws IOException If there was an error reading from the client
+     * @throws InvalidMidiDataException If the client gives invalid MIDI data
+     */
     private void readIn() throws IOException, InvalidMidiDataException {
-        int i = reader.read();
-        System.out.println(i);
-        while(i != HANDSHAKE) // block until we get the handshake from rust
-            i = reader.read();
-        System.out.println("We made it");
-
         char[] buff = new char[4];
-        //add metadata to sequence
-        while(reader.read(buff) != -1) {
-            RustPacket pkt = RustPacket.convert(buff);
-            convertToMsg(pkt);
-        }
-        System.out.println("Do we get here?");
+
+        // Add metadata to sequence
+        while(reader.read(buff) != EOF)
+            convertToMsg(RustPacket.convert(buff));
+
+        System.out.println("Client send EOF");
     }
 
+    /**
+     * Write the MIDI data to the file
+     *
+     * @throws IOException If the write failed
+     */
     private void writeToFile() throws IOException {
         MidiSystem.write(sequence, 1, file);
     }
 
+    /**
+     * Play the MIDI file
+     *
+     * @throws InvalidMidiDataException If the MIDI data is invalid
+     * @throws MidiUnavailableException If the MIDI device could not be opened
+     */
     private void play() throws InvalidMidiDataException, MidiUnavailableException {
         sequencer.setSequence(sequence);
         sequencer.open();
         sequencer.start();
     }
 
-    public static void main(String[] args) {
-        new MidiServer("test.mid").run();
-    }
-
+    /**
+     * Convert a {@link RustPacket} to a MIDI message and add it to the track
+     *
+     * @param pkt the MIDI data packet
+     * @throws InvalidMidiDataException If the MIDI data is invalid
+     */
     private void convertToMsg(RustPacket pkt) throws InvalidMidiDataException {
+        // Add the NOTE_ON message
         ShortMessage msgOn = new ShortMessage();
         msgOn.setMessage(ShortMessage.NOTE_ON, pkt.pitch, pkt.velocity);
         MidiEvent evtOn = new MidiEvent(msgOn, pkt.startTime);
         track.add(evtOn);
 
+        // Add the NOTE_OFF message
         ShortMessage msgOff = new ShortMessage();
         msgOff.setMessage(ShortMessage.NOTE_OFF, pkt.pitch, pkt.velocity);
         MidiEvent evtOff = new MidiEvent(msgOff, pkt.startTime + pkt.duration);
         track.add(evtOff);
     }
-
-
 }
--- src/RustPacket.java
+++ src/RustPacket.java
@@ -1,30 +1,23 @@
-public class RustPacket {
+class RustPacket {
     int pitch;
     int velocity;
     int duration;
     long startTime;
 
-    public RustPacket(int pitch, int velocity, int duration, long startTime) {
+    private RustPacket(int pitch, int velocity, int duration, long startTime) {
         this.pitch = pitch;
         this.velocity = velocity;
         this.duration = duration;
         this.startTime = startTime;
     }
 
-    public RustPacket() {
-        this.pitch = 0;
-        this.velocity = 0;
-        this.duration = 0;
-        this.startTime = 0;
-    }
-
-    public static RustPacket convert(char[] buffer) {
-        RustPacket pkt = new RustPacket();
-        pkt.pitch = buffer[0];
-        pkt.velocity = buffer[1];
-        pkt.duration = buffer[2];
-        pkt.startTime = buffer[3];
-
-        return pkt;
+    /**
+     * Create a {@link RustPacket} from raw data from the client
+     *
+     * @param buffer the raw data
+     * @return the parsed {@link RustPacket}
+     */
+    static RustPacket convert(char[] buffer) {
+        return new RustPacket(buffer[0], buffer[1], buffer[2], buffer[3]);
     }
 }
